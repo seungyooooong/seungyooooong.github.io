bf75957ff9e8ccfef85e2650bb266fe09db5b81c
seungyooooong <chadange@naver.com>
2022-06-20 15:16:57 +0900

    modify main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = new Vue({
+    render: h => h(App)
+}).$mount('#app')
+window.app = app
